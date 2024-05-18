8492411b09e153ab6050744c2ffc667214cffaf5
Akseben <78393586+Akseben@users.noreply.github.com>
2024-05-17 21:26:24 -0500

    Add files via upload

---
+++ Two Sums.cpp
@@ -0,0 +1,30 @@
+// Two Sums.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return { i,j };
+                }
+            }
+        }
+        return {};
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
