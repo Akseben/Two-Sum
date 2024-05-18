9e623f7d676e6088959124da152f1f6787fdea54
Akseben <78393586+Akseben@users.noreply.github.com>
2024-05-17 21:28:47 -0500

    Update Two Sums.cpp

--- Two Sums.cpp
+++ Two Sums.cpp
@@ -9,12 +9,12 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        int n = nums.size();     //commits size to memory to reduce memory usage from grabbing it multiple times during the for loops
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) { //nested for loop, runs through each pair of numbers without comparing the same pos
             for (int j = i + 1; j < n; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return { i,j };
+                    return { i,j };  //returns indices if nums match target value
                 }
             }
         }
